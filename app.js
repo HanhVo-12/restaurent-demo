506b8ef0b99dad5feee8e0d779d71f209f409352
HanhVo-12 <vtdhanh.1712@gmail.com>
2023-05-12 19:08:28 +0700

    upload slider banner

--- app.js
+++ app.js
@@ -4,8 +4,43 @@
 //     window.location.href = "../components/login.html"
 // }
 
+//khai báo biến slideIndex đại diện cho slide hiện tại
+var slideIndex;
+// KHai bào hàm hiển thị slide
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+    //chuyển đến slide tiếp theo
+    slideIndex++;
+    //nếu đang ở slide cuối cùng thì chuyển về slide đầu
+    if (slideIndex > slides.length - 1) {
+        slideIndex = 0
+    }
+    //tự động chuyển đổi slide sau 5s
+    setTimeout(showSlides, 2000);
+}
+//mặc định hiển thị slide đầu tiên 
+showSlides(slideIndex = 0);
+
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+
+
 function addAbout() {
-    window.location.href = ".../components/about.html"
+    window.location.href = "components/about.html"
 }
 
 
